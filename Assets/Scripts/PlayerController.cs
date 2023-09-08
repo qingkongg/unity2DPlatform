fa4d9293bfd7f5cf3958831bc52d3b3e99ae2189
青空 <113504120+qingkongg@users.noreply.github.com>
2023-09-08 16:19:50 +0800

    add moving animation for player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,24 +4,49 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float MoveSpeed = 1.0f;
+
     private Rigidbody2D playerRigibody;
+    private Animator playerAnimator;
+
+    public float MoveSpeed = 1.0f;
+    private bool isRun = false;
     // Start is called before the first frame update
     void Start()
     {
         playerRigibody = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         Run();
+        Flip();
+    }
+
+    void Flip()
+    {
+        if (isRun)
+        {
+            if(playerRigibody.velocity.x > 0.1f)
+            {
+                transform.localRotation = Quaternion.Euler(0 ,0 ,0);
+            }
+            else if (playerRigibody.velocity.x < -0.1f)
+            {
+                transform.localRotation = Quaternion.Euler(0, 180, 0);
+            }
+        }
     }
 
     private void Run()
     {
         float moveDirection = Input.GetAxis("Horizontal");
+        isRun  = Mathf.Abs(moveDirection) > Mathf.Epsilon ;// 一个大于零的最小浮点数
         Vector2 Move = new Vector2(moveDirection * MoveSpeed ,playerRigibody.velocity.y);
         playerRigibody.velocity = Move;
+        playerAnimator.SetBool("IsRun",isRun);
+
     }
 }
